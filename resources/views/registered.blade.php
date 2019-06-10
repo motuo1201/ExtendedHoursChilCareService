740dd11ae119de3f272006d74988e87a0b3eb636
enomoto <y.enomoto@shuhai.co.jp>
2019-06-10 16:11:58 +0900

    登録済みデータの表示画面を追加

---
+++ resources/views/registered.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts\app')
+
+@section('content')
+<div class="container">
+    <h3>既に予定登録済みです。お手数ですが園まで直接ご連絡下さい。</h3>
+    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <h3>ログアウト</h3>
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
+</div>
+@endsection
