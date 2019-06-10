489bd45b890dbdeb9d9db860b6913d78c671294a
enomoto <y.enomoto@shuhai.co.jp>
2019-06-10 12:03:19 +0900

    予定登録の入力チェックまで実装

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Child;
 
 class MainController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
-        if($user->children->count() ==0){
+        if($user->children->count() === 0){
             return view('registrer_children');
         }
         return view('main')->with('user',$user);
+    }
+    public function registerSchedule(Request $request)
+    {
+        $request->validate([
+            'child.*.transferee'       => 'required_with:child.*.estimated_time,child.*.body_temperature',
+            'child.*.estimated_time'   => 'required_with:child.*.transferee,child.*.body_temperature',
+            'child.*.body_temperature' => 'required_with:child.*.transferee,child.*.estimated_time'
+        ]);
+        dd($request);
     }
 
     public function print()
