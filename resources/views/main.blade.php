23aab877b7585421c9c948e56adb3cbf17723a06
enomoto <y.enomoto@shuhai.co.jp>
2019-06-10 12:04:31 +0900

    入力フォームのname属性を扱いやすい様に変更

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,37 +2,47 @@
 
 @section('content')
 <div class="container">
+    @component('layouts.alert-message')
+    @endcomponent
+    @component('layouts.alert-error')
+    @endcomponent
     <h3>{{date('Y/m/d')}}  延長保育申込</h3>
-    <div class="row">
-        <div class="col-md-offset-1 col-md-9 col-s-12">
-            @foreach ($user->children as $children)
+    @foreach ($user->children as $children)
+    <hr>
+    <form class="form" method="POST" action="{{route('registerSchedule')}}">
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-md-offset-1 col-md-9 col-s-12">
                 <h4>
-                氏名:{{$children->family_name}} {{$children->first_name}}
-                クラス:{{$children->class}}{{$children->group}}組
+                    氏名:{{$children->family_name}} {{$children->first_name}}
+                    クラス:{{$children->class}}{{$children->group}}組
                 </h4>
                 <hr>
+    
                 <div class="form-group">
                     <label for="Transferee" class="form-label">お迎えの方</label>
-                    <input class="form-control" type="text" name="transferee" id="transferee"
-                    placeholder="お迎え予定の方のお名前を記入してください">        
+                    <input class="form-control" type="text" name="child[{{$children->id}}][transferee]" id="transferee"
+                           placeholder="お迎え予定の方のお名前を記入してください"
+                           value="{{old('child.' . $children->id. '.transferee')}}">
                 </div>
-                <label for="estimated_time" class="form-label">お迎え時刻と体温</label>
                 <div class="form-group">
                     <div class="col-sm-3">
-                        予定時刻
-                        <input class="form-control" type="time" name="estimated_time" id="estimated_time">
+                        <label for="estimated_time" class="form-label">お迎え予定時刻</label>
+                        <input class="form-control" type="time" name="child[{{$children->id}}][estimated_time]" id="estimated_time"
+                               value="{{old('child.' . $children->id. '.estimated_time')}}">
                     </div>
                     <div class="col-sm-3">
-                        体温
-                        <input class="form-control" type="number" name="body_temperature" id="body_temperature"
-                        placeholder="体温">        
+                        <label for="estimated_time" class="form-label">体温</label>
+                        <input class="form-control" type="number" step="0.1" min="35" max="40" name="child[{{$children->id}}][body_temperature]" id="body_temperature"
+                               placeholder="体温" value="{{old('child.' . $children->id. '.body_temperature')}}">>        
                     </div>
                 </div>
-            @endforeach
-            <div class="row" style="margin-top: 100px">
-                <input type="submit" value="登録する" class="btn btn-primary btn-block">
             </div>
         </div>
-    </div>
+        @endforeach
+        <div class="row" style="margin-top: 100px">
+            <input type="submit" value="登録する" class="btn btn-primary btn-block">
+        </div>
+    </form>
 </div>
 @endsection
