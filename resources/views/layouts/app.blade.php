4c822170a702b1cf66107fb376e71b5930041ccb
enomoto <y.enomoto@shuhai.co.jp>
2019-06-12 13:18:50 +0900

    テーマをピンク中心に変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,81 +11,66 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/datatables.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="./">
+                <img src="assets/img/brand.png" width="30" height="30" class="d-inline-block align-top mr-1" alt="">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            @guest
+                &nbsp;
+            @else
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./">Top <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item dropdown active">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                メニュー
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a  class="dropdown-item" href="{{ route('main') }}">登録</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown active">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    ログアウト
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </div>
+                        </li>
+                        </li>
+                    </ul>
+                </div>
+            @endguest
+        </div>
+    </nav>
+            
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        @guest
-                            &nbsp;
-                        @else
-                            <li class="dropdown">
-                                <a href="#" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    メニュー <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ route('main') }}">登録</a></li>
-                                    <li><a href="{{ route('print') }}" target="_blank">印刷</a></li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            &nbsp;
-                        @else
-                            <li class="dropdown">
-                                <a href="#" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            ログアウト
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
         @yield('content')
     </div>
     <!-- Scripts -->
     <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/datatables.min.js') }}"></script>
     <script src="{{ asset('js/common.js') }}"></script>
     <script src="{{ asset('js/bootstrap-datepicker.min.js') }}"></script>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,42 +6,38 @@
     @endcomponent
     @component('layouts.alert-error')
     @endcomponent
-    <h3>{{date('Y/m/d')}}  延長保育申込</h3>
+    <h3 class="text-primary">{{date('Y/m/d')}}  延長保育申込</h3>
     @foreach ($user->children as $children)
     <hr>
-    <form class="form" method="POST" action="{{route('registerSchedule')}}">
+    <form class="form" method="POST" action="{{route('registerSchedule')}}" class="form-horizontal">
         {{ csrf_field() }}
-        <div class="row">
-            <div class="col-md-offset-1 col-md-9 col-s-12">
-                <h4>
-                    氏名:{{$children->family_name}} {{$children->first_name}}
-                    クラス:{{$children->class}}{{$children->group}}組
-                </h4>
-                <input type="hidden" value="{{$children->id}}" name="child[{{$children->id}}][child_id]">
-                <hr>
-    
-                <div class="form-group">
-                    <label for="Transferee" class="form-label">お迎えの方</label>
-                    <input class="form-control" type="text" name="child[{{$children->id}}][transferee]" id="transferee"
-                           placeholder="お迎え予定の方のお名前を記入してください"
-                           value="{{old('child.' . $children->id. '.transferee')}}">
-                </div>
-                <div class="form-group">
-                    <div class="col-sm-3">
-                        <label for="estimated_time" class="form-label">お迎え予定時刻</label>
-                        <input class="form-control" type="time" name="child[{{$children->id}}][estimated_time]" id="estimated_time"
-                               value="{{old('child.' . $children->id. '.estimated_time')}}">
-                    </div>
-                    <div class="col-sm-3">
-                        <label for="estimated_time" class="form-label">体温</label>
-                        <input class="form-control" type="number" step="0.1" min="35" max="40" name="child[{{$children->id}}][body_temperature]" id="body_temperature"
-                               placeholder="体温" value="{{old('child.' . $children->id. '.body_temperature')}}">>        
-                    </div>
+        <div class="col-md-offset-1 col-md-9 col-s-12">
+            <h4 class="text-primary">
+                氏名:{{$children->family_name}} {{$children->first_name}}
+                クラス:{{$children->class}}{{$children->group}}組
+            </h4>
+            <input type="hidden" value="{{$children->id}}" name="child[{{$children->id}}][child_id]">
+            <hr>
+
+            <div class="form-group">
+                <label for="Transferee" class="control-label">お迎えの方</label>
+                <input class="form-control" type="text" name="child[{{$children->id}}][transferee]" id="transferee"
+                        placeholder="お迎え予定の方のお名前を記入してください"
+                        value="{{old('child.' . $children->id. '.transferee')}}">
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label for="estimated_time" class="control-label col-md-2">お迎え予定</label>
+                    <input class="form-control  col-md-1" type="time" name="child[{{$children->id}}][estimated_time]" id="estimated_time"
+                            value="{{old('child.' . $children->id. '.estimated_time')}}">        
+                    <label for="estimated_time" class="control-label  col-md-1">体温</label>
+                    <input class="form-control  col-md-1" type="number" step="0.1" min="35" max="40" name="child[{{$children->id}}][body_temperature]" id="body_temperature"
+                            placeholder="体温" value="{{old('child.' . $children->id. '.body_temperature')}}">                
                 </div>
             </div>
         </div>
         @endforeach
-        <div class="row" style="margin-top: 100px">
+        <div class="row" style="margin-top: 1em">
             <input type="submit" value="登録する" class="btn btn-primary btn-block">
         </div>
     </form>
